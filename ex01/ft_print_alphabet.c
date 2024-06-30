1e214dd2c4db6f13e4445e3a9fced2933b3f9f27
Allbeth <aleshatrnr@gmail.com>
2024-06-30 20:56:01 +0200

    idk about git

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sarisanc <sarisanc@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/30 20:33:56 by sarisanc          #+#    #+#             */
+/*   Updated: 2024/06/30 20:40:29 by sarisanc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void);
+
+void	ft_print_alphabet(void)
+{
+	write(1,"a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z", 52);
+}
